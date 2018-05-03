f46dc23199f1e8bb541d229d47117f4dac2c81b6
Fedor Paretsky <fedorp1@uw.edu>
2018-05-03 16:09:59 -0700

    Initialize repo

---
+++ app/src/main/java/com/f3d0r/shopifyinternchallenge/MainActivity.java
@@ -0,0 +1,13 @@
+package com.f3d0r.shopifyinternchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
